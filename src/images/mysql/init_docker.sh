daaa8bd94c9e40d80883af6f42642252b104809d
Gabriel <gabrielmartinezrdrgz@gmail.com>
2020-12-22 18:27:53 +0100

    finiquitado

---
+++ src/images/mysql/init_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+telegraf &
+
+nohup ./setup.sh > /dev/null 2>&1 &
+
+mkdir -p /run/mysqld/
+
+sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+/usr/bin/mysql_install_db --user=root --datadir="/var/lib/mysql"
+# /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+/usr/bin/mysqld --user=root --datadir="/var/lib/mysql"
